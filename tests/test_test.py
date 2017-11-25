886193d13983710f38f7ed382593fd7ac1a32df0
vern.li <vern.li@herely.us>
2017-11-25 21:48:10 +0800

    test: delete fail test

--- tests/test_test.py
+++ tests/test_test.py
@@ -3,8 +3,6 @@ import unittest
 class TestUnittest(unittest.TestCase):
     def test_one(self):
         self.assertTrue(True)
-    def test_two(self):
-        self.assertTrue(False)
 
 
 if __name__ == "__main__":
