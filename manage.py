0f251b3736cf4ec8b3bc0119e98c45ae61ff3163
vern.li <vern.li@herely.us>
2018-01-06 20:43:27 +0800

    manage: add command to create or drop database; CI: create database

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ Usage:
     manage.py -t <test_name>
     manage.py -s <script>
     manage.py load_data
+    manage.py db <operate>
 
 
 """
@@ -14,10 +15,10 @@ import os
 import subprocess
 from docopt import docopt
 
-import scripts
 
+from blog import db
 from blog.fixture.load import load_data
-
+import scripts
 
 if __name__ == '__main__':
     argments = docopt(__doc__)
@@ -58,3 +59,13 @@ if __name__ == '__main__':
         print('load test data start')
         load_data()
         print('load test data end')
+    if argments['db']:
+        arg = argments['<operate>']
+        if arg == 'create':
+            print('create database tables start')
+            db.create_all()
+            print('create database tables start')
+        elif arg == 'drop':
+            print('drop database tables start')
+            db.drop_all()
+            print('drop database tables start')
