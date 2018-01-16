cbd9922433e093e303cc565d1301f8c9bda824e7
vern.li <vern.li@herely.us>
2018-01-16 23:47:02 +0800

    :sparkles:简略文章列表api

--- blog/modules/page.py
+++ blog/modules/page.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import re
+
 import markdown
 
 from blog import db
@@ -13,7 +15,7 @@ EXTENSTIONS = ['markdown.extensions.extra',
 
 
 def get_page_list(keyword=None):
-    pages = db.session.query(Page)
+    pages = db.session.query(Page).filter_by(is_show=True)
 
     if keyword:
         patern = '%' + str(keyword) + '%'
@@ -28,3 +30,26 @@ def get_page_detail(id_):
     data = page._todict()
     data['body'] = markdown.markdown(data['body'], extensions=EXTENSTIONS)
     return {'data': data}
+
+
+def get_simple_page_list(index=1, size=20):
+    ''' 获取带简略信息的文章列表 '''
+    pages = db.session.query(Page) \
+              .filter_by(is_show=True) \
+              .order_by(page.use_start.desc())
+
+    total = pages.count()
+
+    pages = pages.slice((index - 1) * size, index * size).all()
+    for page in pages:
+        toc = re.search('\[TOC\]', zxc, re.S)
+        if toc:
+            page.body = page.body[:toc.span()[1]]
+        else:
+            sd_sd_title = re.search('(\#\#.*\#\#{1})', asd, re.S)
+            if sd_sd_title:
+                page.body = page.body[:sd_sd_title.span()[1] - 2]
+    return {'data': [p._todict() for p in pages],
+            'index': index,
+            'size': size,
+            'total': total}
