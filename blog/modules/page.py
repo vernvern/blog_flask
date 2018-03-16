c2474835cddc868dad16e37427901f3d169ed6c5
vern.li <vern.li@herely.us>
2018-03-16 16:26:39 +0800

    :art:

--- blog/modules/page.py
+++ blog/modules/page.py
@@ -94,32 +94,3 @@ def get_page_detail(name):
         return page
 
     raise Exception('居然找不到')
-
-
-def test_coverage():
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
-    a == 1
