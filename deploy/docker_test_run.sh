75ceb8344505bb0dad7b0e2ab2296a65c99f7400
root <root@vultr.guest>
2018-03-20 09:55:58 +0000

    :construction:

--- deploy/docker_test_run.sh
+++ deploy/docker_test_run.sh
@@ -31,10 +31,10 @@ fi
 # rebuild
 if [ "$CI"v = "true"v ]; then
     mkdir /home/travis/build/vernvern/log
-    docker run --name blog_python -d -v /Volumes/log:/opt/log -v /home/travis/build/vernvern/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
+    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
     docker run --name blog_redis -p 6379:6379 -d  --network net --ip 172.18.0.2 blog:redis-4.0.8
 else
     docker run --name blog_redis -p 6379:6379 -d  --network net --ip 172.18.0.2 blog:redis-4.0.8
-    docker run --name blog_python -d -v /Volumes/log:/opt/log -v /Users/vernli/python/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
-    docker run --name blog_nginx -p 80:80 -d -v /Volumes/log:/opt/log --network net --ip 172.18.0.4 blog:nginx-1.13.9
+    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
+    docker run --name blog_nginx -p 80:80 -d -v /opt/log:/opt/log --network net --ip 172.18.0.4 blog:nginx-1.13.9
 fi
