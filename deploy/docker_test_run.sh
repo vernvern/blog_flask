e4ad1dbae579bb2c70faf17d37cf8d0132cb61aa
root <root@vultr.guest>
2018-03-21 06:56:01 +0000

    :whale: docker net

--- deploy/docker_test_run.sh
+++ deploy/docker_test_run.sh
@@ -21,20 +21,18 @@ if [ "$?"v = "0"v ] ; then
     docker rm -f blog_nginx
 fi
 
-# rm network
 (docker network inspect net) > /dev/null 2>&1
 if [ "$?"v != "0"v ] ; then
-    echo 'docker network rm net:'
-    docker network create --subnet=172.18.0.0/16  net
+    docker network create --subnet=192.168.1.1/24  net
 fi
 
 # rebuild
 if [ "$CI"v = "true"v ]; then
     mkdir /home/travis/build/vernvern/log
-    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
-    docker run --name blog_redis -p 6379:6379 -d  --network net --ip 172.18.0.2 blog:redis-4.0.8
+    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src -p 8000:8000 --network net --ip 192.168.1.3 blog:python-3.6.4
+    docker run --name blog_redis -p 6379:6379 -d  --network net --ip 192.168.1.2 blog:redis-4.0.8
 else
-    docker run --name blog_redis -p 6379:6379 -d  --network net --ip 172.18.0.2 blog:redis-4.0.8
-    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src -p 8000:8000 --network net --ip 172.18.0.3 blog:python-3.6.4
-    docker run --name blog_nginx -p 80:80 -d -v /opt/log:/opt/log -v /root/blog:/opt/src --network net --ip 172.18.0.4 blog:nginx-1.13.9
+    docker run --name blog_redis -p 6379:6379 -d  --network net --ip 192.168.1.2 blog:redis-4.0.8
+    docker run --name blog_python -d -v /opt/log:/opt/log -v /root/blog:/opt/src --network net --ip 192.168.1.3 blog:python-3.6.4
+    docker run --name blog_nginx -p 80:80 -d -v /opt/log:/opt/log -v /root/blog:/opt/src --network net --ip 192.168.1.4 blog:nginx-1.13.9
 fi
