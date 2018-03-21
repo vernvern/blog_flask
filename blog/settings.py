039536060395c47531c3170e860553ae7d3ef741
vern.li <vern.li@herely.us>
2018-03-21 15:33:34 +0800

    :construction: log level test

--- blog/settings.py
+++ blog/settings.py
@@ -41,7 +41,7 @@ formatter = logging.Formatter(
 info_file_handler = logging.FileHandler(app.config['LOG_INFO_FILE_PATH'],
                                         encoding='UTF-8')
 info_file_handler.addFilter(app_filter)
-info_file_handler.setLevel(logging.INFO)
+info_file_handler.isEnabledFor(logging.INFO)
 info_file_handler.setFormatter(formatter)
 
 # debug console日志
@@ -85,7 +85,7 @@ def log_api_200(response):
     message = '\n'.join(args)
     log += message
 
-    app.logger.info(message)
+    app.logger.info(log)
     return
 
 
