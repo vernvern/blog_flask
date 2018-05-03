606d1bab27d9852336a80863abb3f727689dbafe
vern.li <vern.li@herely.us>
2018-05-03 23:09:26 +0800

    :sparkles: sort link

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -30,7 +30,7 @@ $("[href='#article']").click(function(){
 $(".sort").click(function(){
     $.post(address +'/api/page/get_page_list',
     {
-        sort=this.text();
+        sort=this.text()
     },
     function(ret){
         $("#article").text("");
