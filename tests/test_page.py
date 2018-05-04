3e499ba706c8b9e42382da5e070d5c3ce245ca13
vern.li <vern.li@herely.us>
2018-05-04 16:55:00 +0800

    :gteen_heart:

--- tests/test_page.py
+++ tests/test_page.py
@@ -11,15 +11,9 @@ class TestCasePage(unittest.TestCase):
     def setUp(self):
         self.keyword = '关键字'
 
-        # test_get_page_detail
-        self.page_name = 'add_xiaobing_in_wechat'
-
     def test_get_page_list(self):
         page.get_page_list()
         page.get_page_list(mode='simple')
-
-    def test_get_page(self):
-        page.get_page(self.page_name)
 
 
 if __name__ == '__main':
