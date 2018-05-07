074904a3cb367f584b74a92fa38937324ff66dcc
vern.li <vern.li@herely.us>
2018-05-07 22:38:44 +0800

    :whale: python容器持久化

--- deploy/Dockerfiles/python/forever.sh
+++ deploy/Dockerfiles/python/forever.sh
@@ -1,11 +1,14 @@
 
-application_run (){
-    while[ 1 ]
-    do
-        if [[ ps | aux | grep 'gunicorn' | wc -l -lt 1 ]];then
-            gunicorn -w 4 -b 127.0.0.1:8000 wsgi:app
-        else
-            sleep 2
+application_run () {
+    while [ 1 ]
+        do
+            line = ps aux | grep 'gunicorn' | wc -l
+            if [[ $line -lt 1 ]];then
+                gunicorn -w 4 -b 127.0.0.1:8000 wsgi:app
+            else
+                sleep 2
+            fi
+        done
 }
 
 
