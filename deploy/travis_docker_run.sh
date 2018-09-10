d1a46731b02db3aa958d6804f90fa26f6c0062ae
vern.li <vern.li@herely.us>
2018-09-10 20:20:24 +0800

    CI: test

---
+++ deploy/travis_docker_run.sh
@@ -0,0 +1,7 @@
+(docker inspect blog_python) > /dev/null 2>&1
+if [ "$?"v = "0"v ] ; then
+    echo 'docker rm blog_python:'
+    docker rm -f blog_python
+if
+
+docker run --name blog_python -d -v /home/travis/build/vernvern/blog/log:/opt/log -v  /home/travis/build/vernvern/blog:/opt/src blog:python-3.6.4
